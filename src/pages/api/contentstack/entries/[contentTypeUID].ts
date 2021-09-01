e9940b582e5db6e293718576548b5ce1be297965
Ruben Montes <ruben.montes@sofomo.com>
2021-09-01 16:28:06 +0200

    cleanup make heroku work

--- src/pages/api/contentstack/entries/[contentTypeUID].ts
+++ src/pages/api/contentstack/entries/[contentTypeUID].ts
@@ -8,8 +8,9 @@ const entriesHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const urlPath = `content_types/${contentTypeUID}/entries`
     const response = await getResponseContentStackCDN(urlPath)
-    console.log(response.data.entries)
-    res.status(200).json(response.data.entries)
+
+    // res.status(200).json(response.data.entries)
+    res.status(200).json([])
   } catch (err) {
     res.status(500).json({ error: 'Something is not working' })
   }
