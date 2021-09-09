df1b6233eca77b3ad9256e670481233b11c1971a
Ruben Montes <ruben.montes@sofomo.com>
2021-09-10 00:52:33 +0200

    refactor landing page

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,113 +1,57 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+
 import React from 'react'
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 
 import { Layout } from '@/components/Layout/Layout'
-import LPIntroduction from '@/components/landing-page/Introduction/Introduction'
-import LPHighlightValues, {
+import Introduction from '@/components/landing-page/Introduction/Introduction'
+import HighlightValues, {
   Value,
 } from '@/components/landing-page/HighlightValues/HighlightValues'
-import LPAdaExperience, {
+import AdaExperience, {
   Experience,
 } from '@/components/landing-page/AdaExperience/AdaExperience'
-import LPHelp from '@/components/landing-page/Help/Help'
-import LPOurMission, {
+import Help from '@/components/landing-page/Help/Help'
+import OurMission, {
   Expert,
 } from '@/components/landing-page/OurMission/OurMission'
-import LPCTAButton from '@/components/landing-page/CTAButton/CTAButton'
+import CTAButton from '@/components/landing-page/CTAButton/CTAButton'
+import Footer from '@/components/landing-page/Footer/Footer'
 
-import LPFooter from '@/components/landing-page/Footer/Footer'
+type LandingPageProps = {
+  values: Value[]
+  experiences: Experience[]
+  topics: string[]
+  experts: Expert[]
+}
 
-const values: Value[] = [
-  {
-    title: 'Backed by science. Grounded in real-world experience.',
-    description:
-      'Problem statement about how it’s hard to find trusted information and how we solve by partnering with expert moms.',
-  },
-  {
-    title: 'Curated content focused on mom, tailored to baby’s age.',
-    description:
-      'Position our copy as solving the problem of you don’t know what you don’t know. We give you the info you need when you need it.',
-  },
-]
+export const getStaticProps: GetStaticProps = async () => {
+  const dataPath = path.join(
+    process.cwd(),
+    'src/pages/landing-page/data/data.json'
+  )
+  const { values, experiences, topics, experts } = JSON.parse(
+    await fs.readFile(dataPath, 'utf8')
+  )
 
-const experiences: Experience[] = [
-  {
-    image: {
-      src: '/images/ada-experience.png',
-      width: 375,
-      height: 513,
-      alt: 'Ada Experience',
+  return {
+    props: {
+      values,
+      experiences,
+      topics,
+      experts,
     },
-    description:
-      'Bite-sized content helps you cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed.',
-  },
-  {
-    image: {
-      src: '/images/ada-experience.png',
-      width: 375,
-      height: 513,
-      alt: 'Ada Experience',
-    },
-    description:
-      'Our comprehensive guides will tell you everything you need to know to etiam porta sem malesuada magna mollis euismod.',
-  },
-  {
-    image: {
-      src: '/images/ada-experience.png',
-      width: 375,
-      height: 513,
-      alt: 'Ada Experience',
-    },
-    description:
-      'Watch, listen and read. Our content is created with context in mind. Aenean lacinia bibendum nulla sed consectetur. ',
-  },
-]
+  }
+}
 
-const topics: string[] = [
-  'Baby Communication',
-  'Development',
-  'Sleep',
-  'Self Care',
-  'Family Care',
-  'Nutrition',
-]
-
-const experts: Expert[] = [
-  {
-    avatar: {
-      src: '/images/expert1.png',
-      width: 50,
-      height: 50,
-      alt: 'Dr. George Russell',
-    },
-  },
-  {
-    avatar: {
-      src: '/images/expert2.png',
-      width: 50,
-      height: 50,
-      alt: 'Dr. Martha Stewart',
-    },
-  },
-  {
-    avatar: {
-      src: '/images/expert3.png',
-      width: 50,
-      height: 50,
-      alt: 'Dr. Martin Campbell',
-    },
-  },
-  {
-    avatar: {
-      src: '/images/expert4.png',
-      width: 50,
-      height: 50,
-      alt: 'Dr. Sarah Smith',
-    },
-  },
-]
-
-export default function LandingPage({}) {
+export default function LandingPage({
+  values,
+  experiences,
+  topics,
+  experts,
+}: LandingPageProps) {
   return (
     <Layout>
       <Head>
@@ -116,13 +60,13 @@ export default function LandingPage({}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <LPIntroduction />
-        <LPHighlightValues values={values} />
-        <LPAdaExperience experiences={experiences} topics={topics} />
-        <LPHelp />
-        <LPOurMission experts={experts} />
-        <LPCTAButton />
-        <LPFooter />
+        <Introduction />
+        <HighlightValues values={values} />
+        <AdaExperience experiences={experiences} topics={topics} />
+        <Help />
+        <OurMission experts={experts} />
+        <CTAButton />
+        <Footer />
       </main>
     </Layout>
   )
