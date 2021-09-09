e79c64f744224dd46d8d49d0389ac5a4e57d4ec7
Ruben Montes <ruben.montes@sofomo.com>
2021-09-10 00:38:52 +0200

    sendgrid integration

---
+++ src/pages/api/mail.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const mailHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const msg = {
+      to: 'ruben.montes@sofomo.com', // Change to your recipient
+      from: 'ruben.montes.mendoza@gmail.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then((response: any) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+      })
+      .catch((error: any) => {
+        console.error(error)
+      })
+    res.status(200).json({ status: 'Ok' })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+}
+
+export default mailHandler
