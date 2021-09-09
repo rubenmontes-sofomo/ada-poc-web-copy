d1dc4bc6c9a22c8d2e8d39e754689b8ea57668eb
Ruben Montes <ruben.montes@sofomo.com>
2021-09-09 08:30:47 +0200

    Link component

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Story, Meta } from '@storybook/react'
+import { withDesign } from 'storybook-addon-designs'
+
+import LinkButton, { LinkProps } from './Link'
+
+export default {
+  component: LinkButton,
+  title: 'Components/Link',
+  decorators: [withDesign],
+} as Meta
+
+const Template: Story<LinkProps> = (args) => <LinkButton {...args} />
+
+export const Overlay = Template.bind({})
+Overlay.args = {
+  text: 'Already a member? Log in',
+  href: '/',
+  overlay: true,
+}
+Overlay.parameters = {
+  backgrounds: {
+    default: 'green',
+  },
+}
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './Link.module.scss'
+
+export type LinkProps = {
+  text: string
+  href: string
+  overlay: boolean
+}
+
+export default function LinkButton({ text, href, overlay }: LinkProps) {
+  return (
+    <Link href={href}>
+      <a className={`${styles.link} ${overlay ? styles.overlay : ''}`}>
+        {text}
+      </a>
+    </Link>
+  )
+}
