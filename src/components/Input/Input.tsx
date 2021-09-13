c91498abbd2b80e3e50a7fea1dabfe58b65c8f11
Ruben Montes <ruben.montes@sofomo.com>
2021-09-13 07:41:05 +0200

    update input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,7 +8,9 @@ export type InputProps = {
   type?: 'text' | 'number' | 'password' | 'file'
   placeholder?: string
   big?: boolean
+  fullWidth?: boolean
   errorMessages?: string[]
+  setValue: (value: string) => void
 }
 
 export default function Input({
@@ -17,19 +19,25 @@ export default function Input({
   type,
   placeholder,
   big = false,
+  fullWidth = false,
   errorMessages,
+  setValue,
 }: InputProps) {
   const input = (
     <>
       <input
         className={`${styles.input} 
           ${big ? styles.big : ''} 
+          ${fullWidth ? styles.fullWidth : ''}
           ${!!label ? styles.withLabel : ''}
           ${errorMessages && !!errorMessages.length ? styles.hasErrors : ''}
         `}
         defaultValue={value}
         type={type}
         placeholder={placeholder}
+        onChange={(e) => {
+          setValue(e.target.value)
+        }}
       />
       {errorMessages &&
         !!errorMessages.length &&
