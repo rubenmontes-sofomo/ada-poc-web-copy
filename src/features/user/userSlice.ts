aa7a9afd963518a12ce7c8a5dd94d6a99464a55f
Ruben Montes <ruben.montes@sofomo.com>
2021-09-13 07:40:50 +0200

    user slice in store

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { RootState } from '../../store'
+
+interface UserState {
+  email: string
+  loggedIn: boolean
+}
+
+const initialState: UserState = {
+  email: '',
+  loggedIn: false,
+}
+
+export const usersSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login(state) {
+      state.loggedIn = true
+    },
+    setEmail(state, action) {
+      state.email = action.payload
+    },
+  },
+})
+
+export const { login, setEmail } = usersSlice.actions
+
+export const selectEmail = (state: RootState) => state.user.email
+
+export default usersSlice.reducer
